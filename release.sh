f3ef9a3cb857cebf3f42916d03bd721af3c6c4f1
Roland Rabien <figbug@gmail.com>
2023-08-31 14:19:04 -0700

    Fix release script

--- release.sh
+++ release.sh
@@ -2,12 +2,8 @@
 set -x
 
 cd "$(dirname "$0")"
-cd ..
 ROOT=$(pwd)
 
-find .
-unzip Binaries/Binaries.zip
-rm Binaries/Binaries.zip
 find .
 
 gh release create "nexus_$GITHUB_REF_NAME" -F Changelist.txt ../Binaries/*.zip
