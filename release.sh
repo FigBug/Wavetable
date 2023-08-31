bae57214786ba216b8d086e0110140ab724edb52
Roland Rabien <figbug@gmail.com>
2023-08-31 16:40:41 -0700

    Hmm

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ set -x
 cd "$(dirname "$0")"
 ROOT=$(pwd)
 
-gh release create "$GITHUB_REF_NAME" -F Changelist.txt ./Binaries/*.zip
+gh release create "$GITHUB_REF_NAME" -F ./Changelist.txt ./Binaries/*.zip
