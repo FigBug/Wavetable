8efd30b6c6cc9daa309fdbbdb67a6c7bb1250939
Roland Rabien <figbug@gmail.com>
2023-08-31 15:02:25 -0700

    More FFS

--- release.sh
+++ release.sh
@@ -4,6 +4,4 @@ set -x
 cd "$(dirname "$0")"
 ROOT=$(pwd)
 
-find .
-
-gh release create "nexus_$GITHUB_REF_NAME" -F Changelist.txt ./Binaries/*.zip
+gh release create "$GITHUB_REF_NAME" -F Changelist.txt ./Binaries/*.zip
