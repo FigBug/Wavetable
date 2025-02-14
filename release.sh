c7b61f69c2cb9896cd15cee10c618bd4c955ffe0
Roland Rabien <figbug@gmail.com>
2025-02-14 10:13:26 -0800

    Fix release

--- release.sh
+++ release.sh
@@ -4,4 +4,5 @@ set -x
 cd "$(dirname "$0")"
 ROOT=$(pwd)
 
-gh release create "$GITHUB_REF_NAME" -F ./Changelist.txt "./Binaries Windows/*.zip" "./Binaries Linux/*.zip" "./Binaries macOS/*.zip"
+find .
+gh release create "$GITHUB_REF_NAME" -F ./Changelist.txt ./Binaries/*.zip
